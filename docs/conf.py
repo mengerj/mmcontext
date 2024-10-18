8ecd7eb127e91702827ba5b7aff087ab1b0a5f7f
mengerj <jonatan.menger@gmx.de>
2024-10-18 11:27:19 +0200

    included torch intersphinx and updated api.md with new classes

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,7 @@ intersphinx_mapping = {
     "anndata": ("https://anndata.readthedocs.io/en/stable/", None),
     "scanpy": ("https://scanpy.readthedocs.io/en/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
+    "torch": ("https://pytorch.org/docs/stable/", None),
 }
 
 # List of patterns, relative to source directory, that match files and
