4d269732fd130f4af4a9c6457f3aaebff4d58dae
mengerj <jonatan.menger@gmx.de>
2024-11-26 14:15:31 +0100

    update evaluation script

--- eval_main.py
+++ eval_main.py
@@ -1,26 +1,29 @@
-import gzip
 import os
-import pickle
 from pathlib import Path
 
 import anndata
 
-from mmcontext.eval import compare_data_properties, scibEvaluator
+from mmcontext.eval import DataProperties, SystemMonitor, scibEvaluator
 from mmcontext.pl.plotting import plot_umap
 from mmcontext.pp import ConfigWorker, consolidate_low_frequency_categories
 
 
-def eval_main(model_out_dir: str):
+def eval_main(pp_hash, train_hash):
     """Main function for evaluating the inferred data obtained from the model."""
+    model_out_dir = Path(f"out/processed/{pp_hash}/model_out/{train_hash}")
+    # Track system usage
+    monitor = SystemMonitor(interval=1)
+    monitor.start()
     # Configure the config worker
     cfg_worker = ConfigWorker()
-    model_out_dir = Path(model_out_dir)
     eval_dir = model_out_dir / "eval"
     fig_dir = eval_dir / "figs"
     # Load the settings used to train the model
     settings_file = model_out_dir / "settings.yaml"
     cfg = cfg_worker.load_config(settings_file)
     data_dir = model_out_dir / "data"
+    # Initialize the property evaluator
+    dp = DataProperties(predefined_subset="microbiome")
     # get all the folders in data_dir (all the inferred data)
     filenames = os.listdir(data_dir)
     for filename in filenames:
@@ -36,11 +39,14 @@ def eval_main(model_out_dir: str):
         res = evaluator.evaluate()
         # save results as csv
         res.to_csv(eval_dir / f"evaluation_{filename}.csv")
+        # To get data properties and compare them, add data to the DataProperties object
+        dp.add_orginal_data(adata.X.toarray(), id=filename)
+        # Get layers with the string "reconstructed" in them
+        reconstructed_keys = [layer for layer in adata.layers if "reconstructed" in layer]
+        # Add reconstructed data to the DataProperties object
+        for reconstructed_key in reconstructed_keys:
+            dp.add_reconstructed_data(adata.layers[reconstructed_key])
 
-        properties = compare_data_properties(adata.X.toarray(), adata.layers["reconstructed"], predefined_subset="all")
-        # save properties as pickle
-        with gzip.open(eval_dir / f"properties_{filename}.pkl.gz", "wb") as f:
-            pickle.dump(properties, f)
         # plot umap
         # Remove low frequency categories for better visualization
         adata = consolidate_low_frequency_categories(
@@ -55,8 +61,26 @@ def eval_main(model_out_dir: str):
                     save_plot=True,
                     save_dir=fig_dir / "umap",
                     nametag=emb_key,
+                    sample_size=5000,
                 )
+    dp.compare_data_properties()
+    # Save log2fc of all properties
+    dp.log2fc_df.to_csv(eval_dir / "log2fc.csv")
+    # Save mean and sdt of the mean log2fc of all properties
+    dp.mean_std_df.to_csv(eval_dir / "mean_std_meanlog2fc.csv")
+    # Plot a pca of the different data sets and their reconstructions
+    dp.plot_pca(save_path=fig_dir / "pca.png")
+    # Plot the metrics as boxplots
+    dp.plot_metrics(save_dir=fig_dir)
+
+    # Stop tracking system metrics
+    monitor.stop()
+    # Save system metrics in text form
+    with open(eval_dir / "system_summary.txt", "w") as f:
+        f.write(monitor.print_summary())
+    # Save metric plots
+    monitor.plot_metrics(save_dir=fig_dir)
 
 
 if __name__ == "__main__":
-    eval_main("out/processed/ca46371a702966c1f7028a1bab4a0433/model_out/01cc22c6ef6ee2ad05f8d61488da0460")
+    eval_main(pp_hash="ca46371a702966c1f7028a1bab4a0433", train_hash="01cc22c6ef6ee2ad05f8d61488da0460")
